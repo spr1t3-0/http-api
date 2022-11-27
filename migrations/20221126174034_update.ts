8e952bf851aa55475dca729bfe3dfaee078b2948
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-11-26 18:46:28 -0600

    Make experienceType not nullable

--- migrations/20221126174034_update.ts
+++ migrations/20221126174034_update.ts
@@ -6,6 +6,19 @@ export async function up(knex: Knex): Promise<void> {
   });
   await knex.schema.alterTable('userExperience', table => {
     table.unique(['userId', 'type']);
+    table.enum('type', [
+      'TOTAL',
+      'GENERAL',
+      'TRIPSITTER',
+      'DEVELOPER',
+      'TEAM',
+      'IGNORED',
+    ], {
+      useNative: true,
+      enumName: 'experience_type',
+    })
+      .alter()
+      .notNullable();
   });
 }
 
@@ -15,5 +28,16 @@ export async function down(knex: Knex): Promise<void> {
   });
   await knex.schema.alterTable('userExperience', table => {
     table.unique(['id', 'userId', 'type']);
+    table.enum('type', [
+      'TOTAL',
+      'GENERAL',
+      'TRIPSITTER',
+      'DEVELOPER',
+      'TEAM',
+      'IGNORED',
+    ], {
+      useNative: true,
+      enumName: 'experience_type',
+    });
   });
 }
