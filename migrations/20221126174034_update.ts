208b07700454821d885036a059928aef3d915c18
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-12-04 13:47:15 -0600

    minor linting

--- migrations/20221126174034_update.ts
+++ migrations/20221126174034_update.ts
@@ -1,15 +1,5 @@
 import { Knex } from 'knex';
 
-// function formatAlterTableEnumSql(tableName: string, columnName: string, enums: string[]): string {
-//   const constraintName = `${tableName}_${columnName}_check`;
-//   const checkValues = enums.map(enu => `'${enu}'::text`);
-//   const checkValuesString = checkValues.join(', ');
-//   return `
-//     ALTER TABLE "${tableName}" DROP CONSTRAINT IF EXISTS "${constraintName}";
-//     ALTER TABLE "${tableName}" ADD CONSTRAINT "${constraintName}" CHECK ("${columnName}" = ANY (ARRAY[${checkValuesString}]));
-//   `;
-// }
-
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('users', table => {
     table.text('lastSeenIn').alter();
