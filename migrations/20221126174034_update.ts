c17a3e80becb7e241b0870a01b837180398e00c3
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2023-01-31 09:23:36 -0600

    RSS + VoiceExp + RPG
    This update includes:
    
        An update to the second migration file because I learned that '.dropUnique' was a thing
        guildRss - adding rss tracking
        voiceExp - changes to the experience table to handle voice experience
        rpg - adds scaffolding for the rpg system
    
    It also includes an update to the user.spec test since some fields were made non-nullable in a previous update

--- migrations/20221126174034_update.ts
+++ migrations/20221126174034_update.ts
@@ -5,17 +5,20 @@ export async function up(knex: Knex): Promise<void> {
     table.text('lastSeenIn').alter();
   });
   await knex.schema.alterTable('userExperience', table => {
+    table.dropUnique(['id', 'userId', 'type']);
     table.unique(['userId', 'type']);
     table.dropNullable('type');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('userExperience', table => {
+    table.setNullable('type');
+    // table.unique(['id', 'userId', 'type']);
+    // table.dropUnique(['userId', 'type']);
+  });
+
   await knex.schema.alterTable('users', table => {
     table.timestamp('lastSeenIn').alter();
-  });
-  await knex.schema.alterTable('userExperience', table => {
-  // table.unique(['id', 'userId', 'type']);
-    table.setNullable('type');
   });
 }
