728bf6beb6eb6779169309b9941652df00f6c8b2
Anonymous <ausfahrt+github@riseup.net>
2023-04-21 13:59:12 +0200

    update migration

--- migrations/20230421112210_add_matrixRoom_to_userTickets.ts
+++ migrations/20230421112210_add_matrixRoom_to_userTickets.ts
@@ -4,6 +4,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('user_tickets', (table) => {
         table.string('matrix_room').nullable();
+        table.string('thread_id').nullable().alter();
       });
 }
 
@@ -11,6 +12,7 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.alterTable('user_tickets', (table) => {
         table.dropColumn('matrix_room');
+        table.string('thread_id').notNullable().alter();
       });
 }   
 
